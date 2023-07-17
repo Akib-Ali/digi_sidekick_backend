5307a6044efeb1230f68c210bbd28ea4e7301eeb
Akib <akib7599@gmail.com>
2023-07-17 19:35:57 +0530

    changes in packeges.js

--- db/conn.js
+++ db/conn.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://akibali:digipassword@cluster0.em7ey5a.mongodb.net/")
+// mongoose.connect("mongodb+srv://akibali:digipassword@cluster0.em7ey5a.mongodb.net/")
+mongoose.connect("mongodb+srv://akib7599:YafieDNY2KyN9aHU@cluster0.hglpxum.mongodb.net/")
