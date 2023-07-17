69424b8c67007519917e9647682a76b2ebc35ac5
Akib <akib7599@gmail.com>
2023-07-17 12:24:21 +0530

    done crud operation in backend

--- routes/router.js
+++ routes/router.js
@@ -8,46 +8,90 @@ const path = require('path')
 //post api 
 router.post("/users", async (req, res) => {
     try {
-      const { user_name, age,position, gender, location } = req.body;
-      console.log(req.body)
-  
-      const newUser = new User({
+        const { user_name, age, position, gender, location } = req.body;
+        console.log(req.body)
+
+        const newUser = new User({
+            user_name,
+            age,
+            position,
+            gender,
+            location,
+            createdAt: Date.now()
+
+        })
+
+        let result = await newUser.save();
+        res.send(result)
+
+    } catch (error) {
+        console.log(error)
+
+    }
+
+})
+
+//get api
+router.get("/users", async (req, res) => {
+    let userlist = await User.find().sort({ createdAt: -1 })
+    if (userlist.length > 0) {
+        res.send(userlist)
+    } else {
+        res.send({ result: "No Result found" })
+    }
+})
+
+
+//delete api 
+router.delete("/user/:_id", async (req, res) => {
+    let deleteselecteduser = await User.deleteOne(req.params)
+    res.send(deleteselecteduser)
+})
+
+//update api 
+
+// router.post("user/:_id", async (req, res) => {
+//     try {
+//         const { user_name, age, position, gender, location } = req.body;
+//         console.log(req.body)
+//         const updatedUser = {
+//             user_name,
+//             age,
+//             position,
+//             gender,
+//             location,
+//             createdAt: Date.now()
+//           };
+//         let result = await User.findByIdAndUpdate(req.params._id, updatedUser, { new: true });
+//         res.send(result);
+
+//     } catch (error) {
+//         console.log(error)
+
+//     }
+router.put("/user/:_id", async (req, res) => {
+    try {
+      const { user_name, age, position, gender, location } = req.body;
+      console.log(req.body);
+      const updatedUser = {
         user_name,
         age,
         position,
         gender,
         location,
         createdAt: Date.now()
-  
-      })
-  
-      let result = await newUser.save();
-      res.send(result)
-  
+      };
+      let result = await User.findByIdAndUpdate(req.params._id, updatedUser, { new: true });
+      res.send(result);
     } catch (error) {
-      console.log(error)
-  
+      console.log(error);
     }
   
-  })
-
-  //get api
-  router.get("/users", async(req, res)=>{
-    let userlist = await User.find().sort({ createdAt: -1 })
-    if (userlist.length > 0) {
-        res.send(userlist)
-      } else {
-        res.send({ result: "No Result found" })
-      }
-  })
 
 
-  //delete api 
-  router.delete("/user/:_id", async (req, res) => {
-    let deleteselecteduser = await User.deleteOne(req.params)
-    res.send(deleteselecteduser)
-  })
-  
+
+
+})
 
 
 module.exports = router
